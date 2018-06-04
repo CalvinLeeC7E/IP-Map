05aabd51cf06c1338421dfb8a3b9451ad3e87320
calvin <llb@yuanbei.biz>
2018-06-05 00:03:34 +0800

    update config

--- vue/config/index.js
+++ vue/config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../../public/dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '',
 
     /**
      * Source Maps
